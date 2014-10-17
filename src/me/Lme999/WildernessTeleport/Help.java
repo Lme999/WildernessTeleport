82984f27448b95b69b42218ee96e7327faf114cc
FletchTech90 <hadenf@outlook.com>
2014-10-17 12:46:00 -0500

    More stuff

---
+++ src/me/Lme999/WildernessTeleport/Help.java
@@ -0,0 +1,13 @@
+package me.Lme999.WildernessTeleport;
+
+import org.bukkit.entity.Player;
+
+public class Help {
+	
+	public Main plugin = new Main();
+	
+	public static void showHelp(Player p){
+		p.sendMessage("");
+	}
+	
+}
--- src/me/Lme999/WildernessTeleport/Main.java
+++ src/me/Lme999/WildernessTeleport/Main.java
@@ -4,6 +4,7 @@ import java.util.logging.Logger;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -25,9 +26,11 @@ public class Main extends JavaPlugin {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		if(cmd.getName().equalsIgnoreCase("wildernessteleport")) {
 			if(args.length == 0){
-				
+				Help.showHelp((Player) sender);
 			} else if(args.length == 1){
-				
+				if(args[0].equalsIgnoreCase("")){
+					
+				}
 			}
 		}
 		return false;
