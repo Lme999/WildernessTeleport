c5b2f6cd0ba8bc1845b1336e271f4d2310595ac2
FletchTech90 <hadenf@outlook.com>
2014-10-17 13:34:25 -0500

    Basic Help Messages

--- src/me/Lme999/WildernessTeleport/Help.java
+++ src/me/Lme999/WildernessTeleport/Help.java
@@ -1,5 +1,7 @@
 package me.Lme999.WildernessTeleport;
 
+import org.bukkit.ChatColor;
+
 import org.bukkit.entity.Player;
 
 public class Help {
@@ -8,7 +10,8 @@ public class Help {
 	
 	public static void showHelp(Player player) {
 		if(Operations.checkPermission("wilderness.help", player)){
-			player.sendMessage("");
+			player.sendMessage(ChatColor.GOLD + "-=[ " + ChatColor.YELLOW + "WildernessTeleport Help" + ChatColor.GOLD + " ]=-");
+			player.sendMessage(ChatColor.GOLD + "/wilderness " + ChatColor.YELLOW + "WildernessTeleport Base Command");
 		} else {
 			player.sendMessage(Messages.noPermission);
 		}
