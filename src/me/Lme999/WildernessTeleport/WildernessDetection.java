2114b8e475908099f58ec5e1ea029f9a392095ee
FletchTech90 <hadenf@outlook.com>
2014-10-17 13:27:12 -0500

    A commit

--- src/me/Lme999/WildernessTeleport/WildernessDetection.java
+++ src/me/Lme999/WildernessTeleport/WildernessDetection.java
@@ -1,9 +1,13 @@
 package me.Lme999.WildernessTeleport;
 
+import org.bukkit.entity.Player;
+
 public class WildernessDetection {
 	
 	Main plugin = new Main();
 	
-	
+	public void checkNearestWilderness(Player p){
+		
+	}
 	
 }
