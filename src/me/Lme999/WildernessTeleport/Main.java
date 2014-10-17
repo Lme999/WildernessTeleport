623ad5123646a8ea725736aeb44448461871cb0f
Lme999 <craftingcrafter@gmail.com>
2014-10-17 11:59:53 -0400

    Add onEnable and onDisable

--- src/me/Lme999/WildernessTeleport/Main.java
+++ src/me/Lme999/WildernessTeleport/Main.java
@@ -1,9 +1,16 @@
 package me.Lme999.WildernessTeleport;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	
 	
+	public void onEnable() {
+		getLogger().info(ChatColor.BLUE + "WildernessTeleport Now Enabled");
+	}
 	
+	public void onDisable() {
+		getLogger().info(ChatColor.RED + "WildernessTeleport Now Disabled!");
+	}
 }
