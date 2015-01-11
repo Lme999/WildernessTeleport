847b16d67800edf2b2b6cbb87a4b18ddd86164f2
Lme999 <craftingcrafter@gmail.com>
2015-01-10 23:48:34 -0500

    Safe position thingy

--- src/me/Lme999/WildernessTeleport/Operations.java
+++ src/me/Lme999/WildernessTeleport/Operations.java
@@ -1,5 +1,7 @@
 package me.Lme999.WildernessTeleport;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 
@@ -21,8 +23,21 @@ public class Operations {
 	
 	public static void teleportSafePosition(Player p, int x, int y, int z) {
 		World w = p.getWorld();
-		
+		if (checkSafePosition(w, x, y, z)) {
+			p.teleport(new Location(w, x, y, z));
+		}
 		w.loadChunk(x, z);
+	}
+	
+	private static boolean checkSafePosition(World w, int x, int y, int z) {
+		Location l = new Location(w, x, y, z);
+		Location lo = new Location(w, x, y+1, z);
+		if (!(l.getBlock().getType().equals(Material.AIR)) && !(lo.getBlock().getType().equals(Material.AIR))) {
+			return true;
+		} else {
+			checkSafePosition(w, x, y+1, z);
+			return true;
+		}
 	}
 	
 }
