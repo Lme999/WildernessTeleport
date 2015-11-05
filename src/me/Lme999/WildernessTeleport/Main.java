f9bcca97be08fbeb1c142cf0d85edb846f76ce6a
Lme999 <craftingcrafter@gmail.com>
2015-11-05 15:36:10 -0500

    Base layout for commands, maybe.

--- src/me/Lme999/WildernessTeleport/Main.java
+++ src/me/Lme999/WildernessTeleport/Main.java
@@ -1,5 +1,8 @@
 package me.Lme999.WildernessTeleport;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -12,7 +15,26 @@ public class Main extends JavaPlugin {
 		getLogger().info("WildernessTeleport is now disabled");
 	}
 	
-	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("wildernessteleport")) {
+			if (sender instanceof Player) {
+				if (args.length == 0) {
+					
+				} else if (args.length == 1) {
+					if (args[0].equalsIgnoreCase("help")) {
+						
+					} else if (args[0].equalsIgnoreCase("?")) {
+						
+					} else if (args[0].equalsIgnoreCase("tppos")) {
+					
+					}
+				}
+			} else {
+				sender.sendMessage("Insert future message here");
+			}
+		}
+		return false;
+	}
 }
 
 
